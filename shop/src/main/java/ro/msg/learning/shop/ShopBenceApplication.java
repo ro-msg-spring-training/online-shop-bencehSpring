2ab9ff4b487fc5e27dac6fec0ead7a53d78d4268
hachb <bencehach@gmail.com>
2020-02-05 14:55:32 +0200

    removed commented code

--- shop/src/main/java/ro/msg/learning/shop/ShopBenceApplication.java
+++ shop/src/main/java/ro/msg/learning/shop/ShopBenceApplication.java
@@ -8,17 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShopBenceApplication {
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/products").allowedOrigins("http://localhost:4200");
-//			}
-//		};
-//	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ShopBenceApplication.class, args);
 	}
